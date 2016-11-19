d905cbb69f8e2ccf60ae7d5bf585220692aefae9
dmitriy_gruzdev <dmitriy_gruzdev@epam.com>
2016-11-20 01:26:24 +0300

    Grid from template

---
+++ testHB.js
@@ -0,0 +1,65 @@
+Handlebars.registerHelper('if_eq', function (a, b, opts) {
+    if (a === b)
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
+
+let struct = {
+    rows: [
+        {
+            cells: [
+                {
+                    type: 'bubble',
+                    value: 80
+                },
+                {
+                    type: 'arrow',
+                    direction: 'right'
+                },
+                {
+                    type: 'bubble',
+                    value: 65
+                }
+            ]
+        },
+        {
+            cells: [
+                {
+                    type: 'arrow',
+                    direction: 'up'
+                },
+                {
+                    type: '',
+                },
+                {
+                    type: 'arrow',
+                    direction: 'up'
+                }
+            ]
+        },
+        {
+            cells: [
+                {
+                    type: 'bubble',
+                    value: 115
+                },
+                {
+                    type: 'arrow',
+                    direction: 'right',
+                    active: true
+                },
+                {
+                    type: 'bubble',
+                    value: 55
+                }
+            ]
+        }
+    ]
+};
+
+let templateHTML = $('#grid-template').html();
+
+let template = Handlebars.compile(templateHTML);
+
+$('.wrapper').append(template(struct));
